fb555bd379645afd639b161793bb81cc1bc316c4
gcg17 <148265914+gcg17@users.noreply.github.com>
2025-02-09 23:28:32 +0100

    update

--- www/UD4/entregaTarea/tareas/listaTareas.php
+++ www/UD4/entregaTarea/tareas/listaTareas.php
@@ -52,26 +52,8 @@ if ($tema == 'dark') {
                         
                         require_once ('../conexiones/mysqli.php');
                         $conexion = getMysqliConnection();
-
-                        /*1) consulta sql de union entre dos tablas sin llamar a la function en mysqli
-                        $sql = "SELECT a.id, a.titulo, a.descripcion, a.estado, u.username 
-                                FROM tareas a INNER JOIN usuarios u ON a.id_usuario = u.id";
-                        $resultado = $conexion -> query($sql);
-                        while ($row = $resultado -> fetch_assoc()) {
-                            echo "<tr>
-                            <td>{$row['id']}</td>
-                            <td>{$row['titulo']}</td>
-                            <td>{$row['descripcion']}</td>
-                            <td>{$row['estado']}</td>
-                            <td>{$row['username']}</td>
-                            <td>
-                            <a class='btn btn-sm btn-outline-success' href='editaUsuarioForm.php?id={$row['id']}' role ='buttom'> Editar </a>
-                            <a class='btn btn-sm btn-outline-danger ms-2' href='borraUsuario.php?id={$row['id']}' role ='buttom'> Borrar </a>
-                            </td>
-                            </tr>";
-                        }*/
                         
-                        # 2) sacar listado llamando a la function listaTareas() en mysqli
+                        #sacar listado llamando a la function listaTareas() en mysqli
                         $resultado = listaTareasUsuario();
                         while ($row = $resultado -> fetch_assoc()) {
                             echo "<tr>
