d1097585e4984317e8eb829c52ee39ee0e0164a9
gcg17 <148265914+gcg17@users.noreply.github.com>
2025-04-13 17:12:57 +0200

    update

--- www/UD6/entregaTarea/index.php
+++ www/UD6/entregaTarea/index.php
@@ -8,6 +8,4 @@ $user = $_ENV['DATABASE_USER'];
 $pass = $_ENV['DATABASE_PASSWORD'];
 
 Flight::register('db', 'PDO', array("mysql:host=$host;dbname=$name", $user, $pass));
-if (Flight::db() != null)  {
-    die("Conexion a la base de datos establecida");
-}
+
